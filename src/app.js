8d29797c8ec256213fd9447acdc18cae80475db2
Shohabbos Usmanov <shohabbosusmanov@gmail.com>
2025-07-27 22:39:59 +0500

    s

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const PORT = 3000;
 const app = express();
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://shopcoreact.netlify.app/",
         credentials: true,
     })
 );
